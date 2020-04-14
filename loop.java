cab73b31d6c9d53bffc0d726a831fed44d67bb0d
nehabalaji <55323591+nehabalaji@users.noreply.github.com>
2020-04-14 22:07:29 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class loop {
+public static void main(String[] args) {
+	int a,b;
+	Scanner sc=new Scanner(System.in);
+	a=sc.nextInt();
+	b=sc.nextInt();
+	if(a>b) {
+		System.out.println("a is greater");
+	}
+	else {
+		System.out.println("b is greater");
+	}
+}
+}
